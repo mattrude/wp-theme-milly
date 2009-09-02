7554e17f22b4b3099783a51708a286bbda8f2c4e
Matt Rude <github@mattrude.com>
2009-09-02 18:55:40 -0500

    Added Yet Another Related Posts Plugin template for theme

---
+++ yarpp-template-milly.php
@@ -0,0 +1,15 @@
+<?php /*
+Example template
+Author: mitcho (Michael Yoshitaka Erlewine)
+*/ 
+?>
+<?php if ($related_query->have_posts()):?>
+<h3>Related Posts</h3>
+<ol>
+	<?php while ($related_query->have_posts()) : $related_query->the_post(); ?>
+	<li><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a><!-- (<?php the_score(); ?>)--></li>
+	<?php endwhile; ?>
+</ol>
+<?php else: ?>
+<p>No related posts.</p>
+<?php endif; ?>
