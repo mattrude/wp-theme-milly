90dcf8e4b23cd705715e0ce5bf33a44530e03f34
Matt Rude <github@mattrude.com>
2009-09-02 19:48:22 -0500

    removed the "No related posts" message.

--- yarpp-template-milly.php
+++ yarpp-template-milly.php
@@ -4,12 +4,11 @@ Author: mitcho (Michael Yoshitaka Erlewine)
 */ 
 ?>
 <?php if ($related_query->have_posts()):?>
+<br />
 <h3>Related Posts</h3>
 <ol>
 	<?php while ($related_query->have_posts()) : $related_query->the_post(); ?>
 	<li><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a><!-- (<?php the_score(); ?>)--></li>
 	<?php endwhile; ?>
 </ol>
-<?php else: ?>
-<p>No related posts.</p>
 <?php endif; ?>
