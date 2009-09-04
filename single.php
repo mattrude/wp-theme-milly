c82ae1b9cc666444c443e33be80077d35d391d4c
Matt Rude <github@mattrude.com>
2009-09-03 23:37:50 -0500

    changed tag attr to byline

--- single.php
+++ single.php
@@ -7,7 +7,7 @@
 	<div class="single"><!--Slightly different styling for single posts and single pages-->
 		<div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 			<h1><?php the_title(); ?></h1>
-			<small class="attr">Written by <?php the_author() ?> on <?php the_time('F jS, Y') ?>, filed under <?php the_category(', ') ?> | <?php the_tags('Tags: ', ', ', ''); ?> | <?php edit_post_link('Edit', ''); ?></small>
+			<small class="byline">Written by <?php the_author() ?> on <?php the_time('F jS, Y') ?>, filed under <?php the_category(', ') ?> | <?php the_tags('Tags: ', ', ', ''); ?> | <?php edit_post_link('Edit', ''); ?></small>
 			<div class="entry">
 				<?php if($post->post_excerpt != '') : ?>
 					<div class="centerer">
