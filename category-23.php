043e2eb0d39b0cd264639c1625ca6ada8a401311
Matt Rude <github@mattrude.com>
2009-09-05 23:49:32 -0500

    Added box around gallery title, and padding around boxes

--- category-23.php
+++ category-23.php
@@ -5,7 +5,9 @@ get_header();
 $cat_title = '<a href="'.get_category_link(intval(get_query_var('cat'))).'">'.single_cat_title('', false).'</a>'; ?>
 
 <div id="content">
-<h1>Matt Rude's Personal Photo Gallery</h1>
+<div id="title" class="post">
+	<h1>Matt Rude's Personal Photo Gallery</h1>
+</div>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<div id="gallerypost-<?php the_ID(); ?>" class="gallerypost post">
