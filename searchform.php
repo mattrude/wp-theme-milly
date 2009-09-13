aeeaefbe039343f89ed02c8c2256ff9b2cba0880
root <root@laptop.mattrude.com>
2009-09-13 00:00:54 -0500

    remove the extra line break in the search form for real this time

--- searchform.php
+++ searchform.php
@@ -3,5 +3,4 @@
   <input type="text" name="s" id="s" onblur="if ( value == '' ) { this.value='Search Pages' }" onfocus="if ( value == 'Search Pages' ) { this.value='' }" tabindex="1" size="22" value="Search Pages"/>
   <input type="hidden" id="searchsubmit" value="Search" />
  </div>
-  <br />
 </form>
