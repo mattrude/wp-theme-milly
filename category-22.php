b557e1da8c3c8971cc3e141e0c8a7473589d4ecc
Matt Rude <github@mattrude.com>
2009-09-15 08:58:45 -0500

    added the_author_posts_link() to twitter category page.

--- category-22.php
+++ category-22.php
@@ -2,7 +2,9 @@
 
 <div id="content">
 	<?php if (have_posts()) : ?>
-	<img src="<?php bloginfo('template_url'); ?>/images/twitter.jpg" class="tweet_post" width="300" height="111" />
+	<div id=tweet_img class="logo-image">
+		<img src="<?php bloginfo('template_url'); ?>/images/twitter.jpg" class="tweet_post" width="300" height="111" />
+	</div>
 	<div id=tweet_head class="post">
 		Welcome to my Twitter feed. Here you will find ALL of my Twitter history.  You may click on the date in the post to go to the origin tweet (if Twitter still has it). You may follow me on Twitter as <a href="http://twitter.com/mdrude">@mdrude</a>.
 	</div>
@@ -16,7 +18,7 @@
 			<div id=tweet_date>
 				<?php global $wp_query; ?>
                         	<?php $tweet_id = get_post_meta( $wp_query->post->ID, 'aktt_twitter_id', true ); ?>
-				Posted to <a href="http://twitter.com">Twitter</a> by <?php the_author() ?> on <a href="http://twitter.com/mdrude/status/<?php echo $tweet_id ?>"><?php the_time('F jS, h:ma T Y ') ?></a>
+				Posted to <a href="http://twitter.com">Twitter</a> by <?php the_author_posts_link(); ?> on <a href="http://twitter.com/mdrude/status/<?php echo $tweet_id ?>"><?php the_time('F jS, h:ma T Y ') ?></a>
 			</div>	
 			</div><!--close post class and post# id-->
 		<?php endwhile; ?>
