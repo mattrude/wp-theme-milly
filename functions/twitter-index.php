3b8ad53980c803f17856f981bf8f0bda815ab5d2
Matt Rude <github@mattrude.com>
2009-10-11 23:37:45 -0500

    updated twitter logos to new bird logo

--- functions/twitter-index.php
+++ functions/twitter-index.php
@@ -1,6 +1,6 @@
 <div <?php post_class(); ?> id="tweet_template">
 	<div id=tweet_post>
-	<img src="<?php bloginfo('template_url'); ?>/images/twitter-logo.jpg" class='tweet-image' width="60" height="60" align='left' style='margin-right: 5px;' />
+	<img src="<?php bloginfo('template_url'); ?>/images/twitter-bird.gif" class='tweet-image' width="60" height="60" align='left' style='margin-right: 5px;' />
 			<?php the_excerpt(); ?>
 	</div>
 	<div id=tweet_date>
