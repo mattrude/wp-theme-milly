6020ede70f705e7fed2137f7f7881dcf6eae0b12
Matt Rude <github@mattrude.com>
2009-10-13 10:53:41 -0500

    * Removed AIM, Jabber, & Yahoo messengers from User Profile page. * changed excerpt length from 55 to 60 * Changed excerpt more text to "..."

--- functions.php
+++ functions.php
@@ -2,6 +2,42 @@
 require_once('functions/control-panel.php');
 require_once('functions/category-excluder.php');
 
+// Add Custom Taxonomies
+add_action( 'init', 'create_my_taxonomies', 0 );
+function create_my_taxonomies() {
+	register_taxonomy( 'people', 'post', array( 'hierarchical' => false, 'label' => 'People', 'query_var' => true, 'rewrite' => true ) );
+	register_taxonomy( 'places', 'post', array( 'hierarchical' => false, 'label' => 'Places', 'query_var' => true, 'rewrite' => true ) );
+	register_taxonomy( 'events', 'post', array( 'hierarchical' => false, 'label' => 'Events', 'query_var' => true, 'rewrite' => true ) );
+}
+
+// Add Custom User Contact Methods
+function add_twitter_contactmethod( $contactmethods ) {
+  // Add Twitter
+  $contactmethods['facebook'] = 'Facebook URL';
+  $contactmethods['twitter'] = 'Twitter';
+ 
+  // Remove Yahoo IM
+  unset($contactmethods['aim']);
+  unset($contactmethods['jabber']);
+  unset($contactmethods['yim']);
+ 
+  return $contactmethods;
+}
+add_filter('user_contactmethods','add_twitter_contactmethod',10,1);
+
+// Changing excerpt length
+function new_excerpt_length($length) {
+	return 60;
+}
+add_filter('excerpt_length', 'new_excerpt_length');
+ 
+// Changing excerpt more
+function new_excerpt_more($more) {
+	return '...';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
+
+// Add Widget sidebar to Theme
 if(function_exists('register_sidebar'))
 	register_sidebar(array (
 		'before_widget' => '<div id="linkcat" class="widget bookmarks widget-bookmarks">',
@@ -10,13 +46,6 @@ if(function_exists('register_sidebar'))
 		'after_title' => '</h3>',
 	));
 	
-add_action( 'init', 'create_my_taxonomies', 0 );
-function create_my_taxonomies() {
-	register_taxonomy( 'people', 'post', array( 'hierarchical' => false, 'label' => 'People', 'query_var' => true, 'rewrite' => true ) );
-	register_taxonomy( 'places', 'post', array( 'hierarchical' => false, 'label' => 'Places', 'query_var' => true, 'rewrite' => true ) );
-	register_taxonomy( 'events', 'post', array( 'hierarchical' => false, 'label' => 'Events', 'query_var' => true, 'rewrite' => true ) );
-}
-
 // Adds robots.txt support
 $defaultrobotstxt = "# This is the default robots.txt file
 User-agent: *
