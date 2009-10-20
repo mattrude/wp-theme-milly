bdbc379825f6e02f9a46a837870c3862de8f3abf
Matt Rude <github@mattrude.com>
2009-10-20 15:17:16 -0500

    added posted by to post-short function page, same as post-full.php

--- functions/post-short.php
+++ functions/post-short.php
@@ -8,12 +8,13 @@
 	<p class="byline">
 		<span class="byline-prep byline-prep-author text">Posted by </span>
 		<span class="author vcard"><?php the_author_posts_link(); ?></span>
-		<span class="byline-prep byline-prep-published text"> on </span>
+		<span class="byline-prep byline-prep-author text"> on </span>
 		<span class="published"><?php the_time('F jS, Y') ?></span>
-		<span class="edit"><?php edit_post_link('Edit', ' | ') ?></span>
+		<span > | Filed Under: <?php the_category(', '); the_tags(' | Tagged as: ', ', ', ''); ?></span>
+		<span><?php edit_post_link('Edit', ' | '); ?></span>
 	</p>
 <?php } ?>
 <div class="entry-content entry">
-		<?php the_excerpt(); ?>
+	<?php the_excerpt(); ?>
 </div><!--close entry class-->
 </div><!--close post class-->
