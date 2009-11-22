e696f88611a02070ce620a812f6b42f3745d23bb
Matt Rude <github@mattrude.com>
2009-11-22 15:23:25 -0600

    move page-twitter.php to page/twitter-page.php

--- pages/twitter-page.php
+++ pages/twitter-page.php
@@ -1,15 +1,12 @@
+<?php /* Template Name: Twitter Page */ ?>
 <?php get_header(); ?>
 
 <div id="content">
 	<?php if (have_posts()) : ?>
 		<!--This is "The Loop"-->
 		<?php while (have_posts()) : the_post();
-			if ( in_category( 'gallery' )) {
-				include('functions/gallery-index.php');
-			} elseif ( in_category( 'tweets' )) {
+			if ( in_category( 'tweets' )) {
 				include('functions/twitter-index.php');
-			} else {
-				include('functions/post-full.php');
 			}
 		endwhile; ?>
 		<!--The Loop has ended-->	
