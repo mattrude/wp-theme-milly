2d62f819c11c6784fc3884e6eb175d54c61ee242
Matt Rude <github@mattrude.com>
2009-11-22 15:59:40 -0600

    Updates to twitter page

--- twitter-page.php
+++ twitter-page.php
@@ -2,22 +2,14 @@
 <?php get_header(); ?>
 
 <div id="content">
-	<?php if (have_posts()) : ?>
-		<!--This is "The Loop"-->
-		<?php while (have_posts()) : the_post();
-			if ( in_category( 'gallery' )) {
-				include('functions/gallery-index.php');
-			} elseif ( in_category( 'tweets' )) {
-				include('functions/twitter-index.php');
-			} else {
-				include('functions/post-full.php');
-			}
-		endwhile; ?>
-		<!--The Loop has ended-->	
-		<div class="navigation">
-			<div class="txtalignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
-			<div class="txtalignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
-		</div>
+	 <?php query_posts('category_name=tweets&posts_per_page=20');
+	if (have_posts()) : while (have_posts()) : the_post();
+		include('functions/twitter-index.php');
+	endwhile; ?>
+	<div class="navigation">
+		<div class="txtalignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
+		<div class="txtalignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+	</div>
 	<?php endif; ?>
 </div><!--close content id-->
 
