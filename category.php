bf781763e020530502cb3ce1b6392247a3bdbeb0
Matt Rude <m@mattrude.com>
2009-12-05 18:39:31 -0600

    made the dumb thing work again

--- category.php
+++ category.php
@@ -1,26 +1,23 @@
 <?php get_header(); ?>
 
 <div id="content">
-	<?php if (have_posts()) : ?>
-		<div class="author-head post">
-			<h1>Posts Filed Under: <?php single_cat_title(); ?></h1>
-		</div>
-		<!--This is "The Loop"-->
-		<?php while (have_posts()) : the_post(); ?>
-		<? if ( is_category( 'gallery' )) {
-			include('functions/gallery-index.php');
-                } elseif ( is_category( 'tweets' )) {
-			include('functions/twitter-index.php');
-                } else {
-			include('functions/post-full.php');
-		}
-		endwhile; ?>
-		<!--The Loop has ended-->	
-		<div class="navigation">
-			<div class="txtalignleft"><?php previous_posts_link('&laquo; Newer Entries') ?></div>
-			<div class="txtalignright"><?php next_posts_link('Older Entries &raquo;') ?></div>
-		</div>
-	<?php endif; ?>
+  <div class="author-head post">
+    <h1>Posts Filed Under: <?php single_cat_title(); ?></h1>
+  </div>
+  <!--This is "The Loop"-->
+  <?php while (have_posts()) : the_post();
+    if ( is_category( 'gallery' )) {
+      include('functions/gallery-index.php');
+    } elseif ( is_category( 'tweets' )) {
+      include('functions/twitter-index.php');
+    } else {
+      include('functions/post-full.php');
+    } endwhile; ?>
+  <!--The Loop has ended-->	
+  <div class="navigation">
+    <div class="txtalignleft"><?php previous_posts_link('&laquo; Newer Entries') ?></div>
+    <div class="txtalignright"><?php next_posts_link('Older Entries &raquo;') ?></div>
+  </div>
 </div><!--close content id-->
 
 <?php get_sidebar(); ?>
---
+++ navigation.php
@@ -0,0 +1,5 @@
+<div class="navigation post">
+  <div class="floatleft"><?php previous_post_link('&laquo; %link') ?></div>
+  <div class="floatright"><?php next_post_link('%link &raquo;') ?></div>
+  <div class="clearfloatthick">&nbsp;</div>
+</div>
