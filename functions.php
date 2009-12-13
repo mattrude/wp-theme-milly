0b5c7a2e90ef8940793d0437cff2b51c4e9ebcde
Matt Rude <m@mattrude.com>
2009-12-13 03:03:17 -0600

    added time since shortcode function from base repo

--- functions.php
+++ functions.php
@@ -60,6 +60,29 @@ if(function_exists('register_sidebar'))
 		'after_title' => '</h3>',
 	));
 	
+/*
+This shortcode displays the years since the date provided.
+To use this shortcode, add some text to a post or page simmiler to:
+
+    [ts date='1983-09-02']
+
+The date format is YYYY-MM-DD
+*/
+
+function mdr_timesince($atts, $content = null) {
+  extract(shortcode_atts(array("date" => ''), $atts));
+  if(empty($date)) {
+    return "<br /><br />************No date provided************<br /><br />";
+  }
+  $mdr_unix_date = strtotime($date);
+  $mdr_time_difference = time() - $mdr_unix_date ;
+  $years = round($mdr_time_difference / 31556926 );
+  $num_years_since = $years;
+  return $num_years_since;
+}
+
+add_shortcode('ts', 'mdr_timesince');
+
 $Options =
 array
 (
