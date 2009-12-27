2b2bfb3ed0b82d5c1a3af09decb2ff2e79bcd3b9
Matt Rude <m@mattrude.com>
2009-12-27 10:48:23 -0600

    more random image updates

--- functions/random-image.php
+++ functions/random-image.php
@@ -7,8 +7,8 @@ class random_image_widget extends WP_Widget {
       $moFile = dirname(__FILE__) . "/languages/random_image_widget_" .  $currentLocale . ".mo";
       if(@file_exists($moFile) && is_readable($moFile)) load_textdomain('', $moFile);
     }
-    $random_image_widget_name = __('Random Image Widget', 'random_image_widget');
-    $random_image_widget_description = __('Displays a random gallery image from the WordPress built in galleries.', 'random_image_widget');
+    $random_image_widget_name = __('Random Image Widget', 'mdr_random_image_widget');
+    $random_image_widget_description = __('Displays a random gallery image from the WordPress built in galleries.', 'mdr_random_image_widget');
     $widget_ops = array('classname' => 'random_image_widget', 'description' => $random_image_widget_description );
     $this->WP_Widget('random_image_widget', $random_image_widget_name, $widget_ops);
   }  
@@ -18,8 +18,6 @@ class random_image_widget extends WP_Widget {
     $image_height = empty($instance['image_height']) ? '&nbsp;' : apply_filters('image_height', $instance['image_height']);
     global $wpdb;
     
-    //http://codex.wordpress.org/Template_Tags/get_posts
-    //http://codewordpress.com/wordpress-hack/display-random-images-from-media-gallery
     $args = array(
        'post_type' => 'attachment',
        'post_mime_type' => 'image',
@@ -71,8 +69,12 @@ class random_image_widget extends WP_Widget {
   
   function form($instance) {
     $posts_per_page = strip_tags($instance['image_height']);
+    $posts_per_page = strip_tags($instance['image_width']);
+    $posts_per_page = strip_tags($instance['gallery_category']);
     ?>
-    <p><label for="<?php echo $this->get_field_id('image_height'); ?>"><?php _e('Image Height in pix', 'image_height')?>:<input class="widefat" id="<?php echo $this->get_field_id('image_height'); ?>" name="<?php echo $this->get_field_name('image_height'); ?>" type="text" value="<?php echo attribute_escape($image_height); ?>" /></label></p>
+    <p><label for="<?php echo $this->get_field_id('image_height'); ?>"><?php _e('Image Height in pix', 'mdr_random_image_widget')?>:<input class="widefat" id="<?php echo $this->get_field_id('image_height'); ?>" name="<?php echo $this->get_field_name('image_height'); ?>" type="text" value="<?php echo attribute_escape($image_height); ?>" /></label></p>
+    <p><label for="<?php echo $this->get_field_id('image_width'); ?>"><?php _e('Image Width in pix', 'mdr_random_image_widget')?>:<input class="widefat" id="<?php echo $this->get_field_id('image_width'); ?>" name="<?php echo $this->get_field_name('image_width'); ?>" type="text" value="<?php echo attribute_escape($image_height); ?>" /></label></p>
+    <p><label for="<?php echo $this->get_field_id('gallery_category'); ?>"><?php _e('Your Gallery Category', 'mdr_random_image_widget')?>:<input class="widefat" id="<?php echo $this->get_field_id('gallery_category'); ?>" name="<?php echo $this->get_field_name('gallery_category'); ?>" type="text" value="<?php echo attribute_escape($image_height); ?>" /></label></p>
     <?php
   }
   
