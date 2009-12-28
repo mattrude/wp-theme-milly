d7126f8b5e06f15ee773be8427cc09a823500dfd
Matt Rude <m@mattrude.com>
2009-12-28 00:09:25 -0600

    added category option to widget control

--- functions/random-image.php
+++ functions/random-image.php
@@ -15,54 +15,45 @@ class random_image_widget extends WP_Widget {
   
   function widget($args, $instance) {
     extract($args);
-    $mdr_random_image_widget_title = empty($instance['widget_title']) ? '&nbsp;' : apply_filters('widget_title', $instance['widget_title']);
+    $riw_widget_title = empty($instance['widget_title']) ? '&nbsp;' : apply_filters('widget_title', $instance['widget_title']);
+    $riw_cat_slug = empty($instance['gallery_category']) ? 'empty' : apply_filters('gallery_category', $instance['gallery_category']);
     global $wpdb;
-    
-    if ($mdr_random_image_widget_title == "&nbsp;") {
-      $mdr_random_image_widget_title = __('Random Image','mdr_random_image_widget');
+
+    if ($riw_widget_title == "&nbsp;") {
+      $riw_widget_title = __('Random Image','mdr_random_image_widget');
     }
-    
+
     $args = array(
        'post_type' => 'attachment',
        'post_mime_type' => 'image',
        'numberposts' => -1,
        'post_status' => null,
-       'post_category' => 23,
        'post_parent' => $post->ID,
        'orderby' => 'rand'
     );
+    $riw_cat_id = get_category_by_slug($riw_cat_slug);
     $attachments = get_posts($args);
     $noimages = count($attachments);
      
     if ($attachments) {
       foreach ($attachments as $attachment) {
-        $imgtitle = $attachment->post_title;
-        $caption = $attachment->post_excerpt;
-        $imgid = $attachment->ID;
-        $fileurl = $attachment->guid;
         $albumid = $attachment->post_parent;
-        $albumtitle = get_the_title($albumid);
-         
-        $meta = wp_get_attachment_metadata($imgid);
-        $imgw = $meta['sizes']['thumbnail']['width'];
-        $imgh = $meta['sizes']['thumbnail']['height'];
-          
-        $imgext = substr($fileurl, -4);
-        $fileurl = substr($fileurl, 0, -4);
-        $fileurl = $fileurl."-".$imgw."x".$imgh.$imgext;
-         
-        // construct the image
-        echo "<div class='widget bookmarks widget-bookmarks'>";
-          echo "<h3 class='widget-title' >$mdr_random_image_widget_title</h3>";
-          echo "<div class='random-widget'>";
-            echo "<a href=".get_permalink( $imgid )." >";
-            echo "<img src='".$fileurl."' alt='".$imgtitle."' class='center' />";
-            echo "</a>";
-            echo "<p><center><strong>$caption</strong></center></p>";
-            echo "<p><small>Album: <a href=".get_permalink( $albumid ).">$albumtitle</a></small></p>";
+        if ( in_category( $riw_cat_id, $albumid )) { 
+          $imgid = $attachment->ID;
+
+          // construct the image
+          echo "<div class='widget bookmarks widget-bookmarks'>";
+            echo "<h3 class='widget-title' >$riw_widget_title</h3>";
+            echo "<div class='random-widget'>";
+              echo "<a href=".get_permalink( $imgid )." >";
+              echo "<img src='".wp_get_attachment_thumb_url($imgid)."' alt='".$attachment->post_title."' class='center' />";
+              echo "</a>";
+              echo "<p id='riw-caption'><strong>$attachment->post_excerpt</strong></p>";
+              echo "<p id='riw-album'><small>".__('Album:','mdr_random_image_widget')." <a href=".get_permalink( $albumid ).">".get_the_title($albumid)."</a></small></p>";
+            echo "</div>";
           echo "</div>";
-        echo "</div>";
-        break;
+          break;
+	}
       }
     }
   }
@@ -70,13 +61,16 @@ class random_image_widget extends WP_Widget {
   function update($new_instance, $old_instance) {
     $instance = $old_instance;
     $instance['widget_title'] = strip_tags($new_instance['widget_title']);
+    $instance['gallery_category'] = strip_tags($new_instance['gallery_category']);
     return $instance;
   }
   
   function form($instance) {
-    $mdr_random_image_widget_title = strip_tags($instance['widget_title']);
-    ?><p><label for="<?php echo $this->get_field_id('widget_title'); ?>"><?php _e('Widget title', 'mdr_random_image_widget')?>:<input class="widefat" id="<?php echo $this->get_field_id('widget_title'); ?>" name="<?php echo $this->get_field_name('widget_title'); ?>" type="text" value="<?php echo attribute_escape($mdr_random_image_widget_title); ?>" /></label></p>
-    <?php
+    $riw_widget_title = strip_tags($instance['widget_title']);
+    $riw_cat_slug = strip_tags($instance['gallery_category']);
+    ?><p><label for="<?php echo $this->get_field_id('widget_title'); ?>"><?php _e('Widget title', 'mdr_random_image_widget')?>:<input class="widefat" id="<?php echo $this->get_field_id('widget_title'); ?>" name="<?php echo $this->get_field_name('widget_title'); ?>" type="text" value="<?php echo attribute_escape($riw_widget_title); ?>" /></label></p><?php
+
+    ?><p><label for="<?php echo $this->get_field_id('gallery_category'); ?>"><?php _e('Gallery Category slug (only one)', 'mdr_random_image_widget')?>:<input class="widefat" id="<?php echo $this->get_field_id('gallery_category'); ?>" name="<?php echo $this->get_field_name('gallery_category'); ?>" type="text" value="<?php echo attribute_escape($riw_cat_slug); ?>" /></label></p><?php
   }
   
 }
