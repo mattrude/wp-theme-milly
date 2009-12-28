686a4accd650bd0819efd09e7243d39361d9e0ac
Matt Rude <m@mattrude.com>
2009-12-28 09:17:52 -0600

    updated the 404 error page

--- 404.php
+++ 404.php
@@ -1,15 +1,14 @@
 <?php get_header(); ?>
 
 <div id="content">
-		<div class="post">
-			<h2 class="center">Oops! Couldn't find what you were looking for.</h2>
-			<p class="center">Sorry, but you are looking for something that isn't here.</p>
-			<?php include (TEMPLATEPATH . "/searchform.php"); ?>
-		</div>
+  <div class="page">
+    <h2 class="center"><center>Oops! - 404 Error</center></h2> 
+    <p class="center"><center>No sense sticking around here for very long. Check out the sidebar where you will be able to search, or checkout my <a href="http://mattrude.com/category/gallery/">Photo Gallery</a> or <a href="http://mattrude.com/category/tech/">Technology</a> section if your into those sorts of things. If you feel you've reached this page as an error on my part, please <a href="http://mattrude.com/contact-me/">contact me</a>.</center></p>
+    <center><img src="<?php bloginfo('template_url') ?>/images/404.jpg"></center>
+
+  </div>
 </div>
 
 <?php get_sidebar(); ?>
-
 <?php get_footer(); ?>
-
 </div>
