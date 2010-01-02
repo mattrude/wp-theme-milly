70353c39deca0698a24d66963d4b7204e2144da4
Matt Rude <m@mattrude.com>
2010-01-01 22:26:44 -0600

    updates to twitter page

--- functions/twitter-index.php
+++ functions/twitter-index.php
@@ -3,7 +3,7 @@
 	<img src="<?php bloginfo('template_url'); ?>/images/twitter-bird.png" class='tweet-image' width="60" height="60" style='margin-right: 5px;' alt='Twitter bird' />
 			<?php the_content(); ?>
 	</div>
-	<div id='tweet_date-<?php echo $wp_query->post->ID; ?>' class='tweet_date' >
+	<div id='tweet_date-<?php echo $wp_query->post->ID; ?>' class='byline tweet_date' >
 		<?php global $wp_query; ?>
 	 	<?php $tweet_id = get_post_meta( $wp_query->post->ID, 'aktt_twitter_id', true ); ?>
 		Posted to <a href="http://twitter.com">Twitter</a> by <?php the_author_posts_link(); ?> on <a href="http://twitter.com/mdrude/status/<?php echo $tweet_id ?>"><?php the_time('F jS, h:ma T Y ') ?></a>
