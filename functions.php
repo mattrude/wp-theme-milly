f05413cfe414e93e8c5ff94275f8af3f9a775b9c
Matt Rude <m@mattrude.com>
2010-02-21 11:25:29 -0600

    minor code clean up, confirm some functions excits before running them, rename others to themes name.

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 require_once('functions/control-panel.php');
 require_once('functions/category-excluder.php');
 require_once('functions/google-analytics.php');
+//require_once('functions/google-webmaster-tools.php');
 require_once('functions/random-image-block.php');
 require_once('functions/regenerate-thumbnails.php');
 require_once('functions/robots.php');
@@ -13,26 +14,30 @@ if (function_exists(add_theme_support)) {
   set_post_thumbnail_size(200, 200);
 }
 
-// Add Custom Taxonomies
-function create_my_taxonomies() {
-	register_taxonomy( 'people', 'post', array( 'hierarchical' => false, 'label' => 'People', 'query_var' => true, 'rewrite' => true ) );
-	register_taxonomy( 'places', 'post', array( 'hierarchical' => false, 'label' => 'Places', 'query_var' => true, 'rewrite' => true ) );
-	register_taxonomy( 'events', 'post', array( 'hierarchical' => false, 'label' => 'Events', 'query_var' => true, 'rewrite' => true ) );
-        }
-add_action( 'init', 'create_my_taxonomies', 0 );
+// Add Custom Taxonomies for WordPress 2.9
+if (function_exists(register_taxonomy)) {
+  function create_milly_taxonomies() {
+    register_taxonomy( 'people', 'post', array( 'hierarchical' => false, 'label' => 'People', 'query_var' => true, 'rewrite' => true ) );
+    register_taxonomy( 'places', 'post', array( 'hierarchical' => false, 'label' => 'Places', 'query_var' => true, 'rewrite' => true ) );
+    register_taxonomy( 'events', 'post', array( 'hierarchical' => false, 'label' => 'Events', 'query_var' => true, 'rewrite' => true ) );
+  }
+  add_action( 'init', 'create_milly_taxonomies', 0 );
+}
 
-// Add Custom Post Types
-function milly_post_type_init() {
-        register_post_type('twitter', array('label' => __('Twitter'), 'exclude_from_search' => true) );
-	register_taxonomy_for_object_type('post_tag', 'twitter');
-        }
-add_action('init','milly_post_type_init');
+// Add Custom Post Types for WordPress 2.9
+if (function_exists(register_post_type)) {
+  function milly_post_type_init() {
+    register_post_type('twitter', array('label' => __('Twitter'), 'exclude_from_search' => true) );
+    register_taxonomy_for_object_type('post_tag', 'twitter');
+  }
+  add_action('init','milly_post_type_init');
+}
 
 // Add Custom User Contact Methods
-function add_twitter_contactmethod( $contactmethods ) {
+function add_milly_contactmethod( $contactmethods ) {
   // Add Twitter
   $contactmethods['facebook'] = 'Facebook URL';
-  $contactmethods['twitter'] = 'Twitter';
+  $contactmethods['twitter'] = 'Twitter ID';
  
   // Remove Yahoo IM
   unset($contactmethods['aim']);
@@ -41,7 +46,7 @@ function add_twitter_contactmethod( $contactmethods ) {
  
   return $contactmethods;
 }
-add_filter('user_contactmethods','add_twitter_contactmethod',10,1);
+add_filter('user_contactmethods','add_milly_contactmethod',10,1);
 
 // Changing excerpt length
 function new_excerpt_length($length) {
