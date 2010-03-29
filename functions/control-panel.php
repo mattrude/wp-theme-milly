855c748818b4eea7515f73205972519b6a3235f7
Matt Rude <m@mattrude.com>
2010-03-29 11:42:29 -0500

    Put the Control Panel under the wrap <div>

--- functions/control-panel.php
+++ functions/control-panel.php
@@ -91,10 +91,10 @@ class ControlPanel
 	/* Print the options page */
 	function OptionsMenu()
 	{
-		echo '<div class="ajaxmessage updated fade below-h2 message"><p></p></div>';
+		echo '<div class="wrap">';
 		echo '<div class="icon32" id="icon-themes"></div><h2>' . $this->Name . ' Settings</h2>';
 		echo '<div class="wrap" id="poststuff">';
-		if ( $_REQUEST['saved'] ) echo '<div id="message" class="updated fade below-h2" style="background-color: rgb(255, 251, 204); margin-bottom:16px;"><p style="font-style:normal;font-size:13px;">' . $this->Name . ' Settings Saved Successfully.</p></div>';
+		if ( $_REQUEST['saved'] == true ) echo '<div id="message" class="updated fade below-h2" style="background-color: rgb(255, 251, 204); margin-bottom:16px;"><p style="font-style:normal;font-size:13px;">' . $this->Name . ' Settings Saved Successfully.</p></div>';
 		echo '<form method="post" action="themes.php?page=' . basename(__FILE__) . '" id="settings">';
 		$Settings = $this->Settings;
 		foreach( $this->Options as $Option)
