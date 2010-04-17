5151bd430adda0072b3cf5647c98011c36f38090
Matt Rude <m@mattrude.com>
2010-04-17 14:40:47 -0500

    removed broken test twitter redirect

--- functions.php
+++ functions.php
@@ -58,24 +58,6 @@ function milly_post_type_init() {
 }
 add_action('init','milly_post_type_init');
 
-add_action('template_redirect', 'milly_template_redirect');
- 
-function milly_template_redirect()
-{
-  global $wp;
-  global $wp_query;
-  if ($wp->query_vars['post_type'] == 'twitter')
-  {
-    if (have_posts())
-    {
-      include(TEMPLATEPATH . '/twitter.php');
-      die();
-    } else {
-      $wp_query->is_404 = true;
-    }
-  }
-}
-
 // Add Custom User Contact Methods
 function add_milly_contactmethod( $contactmethods ) {
   // Add Twitter
