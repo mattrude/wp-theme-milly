74f676ad36272384cbfd214aa056968f2afb2407
Matt Rude <m@mattrude.com>
2010-04-18 15:29:31 -0500

    updated lower nav bar

--- navigation-category.php
+++ navigation-category.php
@@ -1,5 +1,17 @@
+<?php
+$next = next_posts_link('1');
+$previous = previous_posts_link('1');
+if ($next == 1) {
+  $nav = '1';
+}
+
+if ($previous == 1) {
+  $nav = '1';
+}
+if ($nav == '1') { ?>
   <div class="navigation">
     <div class="txtalignleft"><?php previous_posts_link('&laquo; Newer Entries'); ?></div>
     <div class="txtalignright"><?php next_posts_link('Older Entries &raquo;') ?></div>
     <div class="clearfloatthick">&nbsp;</div>
   </div>
+<?php } ?>
