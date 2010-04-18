a685b3856867db477ecac0d7e5c6ac10a4ac6b02
Matt Rude <m@mattrude.com>
2010-04-18 00:37:24 -0500

    updated twitter posts

--- functions/twitter-post-type.php
+++ functions/twitter-post-type.php
@@ -5,15 +5,9 @@ global $Panel;
 $TwitterEnabled = $Panel->Settings('TwitterEnabled');
 $TwitterCategory = $Panel->Settings('TwitterCategory');
 
-//echo $TwitterEnabled;
-//echo $TwitterCategory;
-
 if ( $TwitterEnabled == 'true' ) {
-  //echo " enabled";
   if ( $TwitterCategory ) {
-    //echo " set";
     if ( $TwitterCategory != 1 ) {
-      //echo " !=1 - ";
       function milly_twitter_post_type() {
         global $wpdb, $Panel;
         $TwitterCategory = $Panel->Settings('TwitterCategory');
@@ -26,9 +20,11 @@ if ( $TwitterEnabled == 'true' ) {
         $mdr_postslist = get_posts($args);
         foreach ($mdr_postslist as $post) {
           $mdr_postid = $post->ID;
-    //      echo $mdr_postid;
-          $wpdb->query("UPDATE $wpdb->posts SET post_type = 'twitter' WHERE ID = $mdr_postid");
-          $wpdb->query("UPDATE $wpdb->term_taxonomy SET count = 0 WHERE term_id = '$TwitterCategory'");
+          if ($mdr_postid > 0) {
+            $wpdb->query("UPDATE $wpdb->posts SET post_type = 'twitter' WHERE ID = $mdr_postid");
+            $wpdb->query("DELETE FROM $wpdb->term_relationships WHERE object_id = $mdr_postid");
+            $wpdb->query("UPDATE $wpdb->term_taxonomy SET count = 0 WHERE term_id = '$TwitterCategory'");
+          }
         }
       }
       milly_twitter_post_type();
