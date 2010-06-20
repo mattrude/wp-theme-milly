eb8405dd924d54174d70af2ac8611478345ac955
Matt Rude <m@mattrude.com>
2010-06-20 16:00:50 -0500

    added quots to google_analytics() & google_webmaster_tools() to header-simple.php

--- header-simple.php
+++ header-simple.php
@@ -10,7 +10,7 @@
     <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-    <?php if (function_exists(google_webmaster_tools)) { google_webmaster_tools(); } ?>
+    <?php if (function_exists('google_webmaster_tools')) { google_webmaster_tools(); } ?>
     <!--This is a plugin hook-->
     <?php wp_head(); ?>
 
@@ -18,7 +18,7 @@
 
 <body>
 
-<?php if (function_exists(google_analytics)) { google_analytics(); } ?>
+<?php if (function_exists('google_analytics')) { google_analytics(); } ?>
 
 <div id="body-container">
 		<div id="header-container">
