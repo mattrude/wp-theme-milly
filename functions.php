8259c33c8ab8e8f6798f9d32697c9e2198e7a33e
Matt Rude <m@mattrude.com>
2010-11-28 16:43:21 -0600

    Added technology post_type tag & category widgets, see #4

--- functions.php
+++ functions.php
@@ -34,17 +34,90 @@ register_post_type('technology', array(
 	'taxonomies' => array('category', 'post_tag')
 ));
 
+// Create the Technology Tag & Category taxonomies.
 function create_technology_taxonomies() {
   register_taxonomy( 'tech_category', 'technology', array( 'hierarchical' => true, 'label' => __('Category'), 'query_var' => true, 'rewrite' => true ) );
   register_taxonomy( 'tech_tag', 'technology', array( 'hierarchical' => false, 'label' => __('Tag'), 'query_var' => true, 'rewrite' => true ) );
 }
 add_action( 'init', 'create_technology_taxonomies', 0 );
 
-//add_action('init', 'technology_add_default_boxes');
-//function technology_add_default_boxes() {
-//    register_taxonomy_for_object_type('tech_category', 'technology');
-//    register_taxonomy_for_object_type('tech_tag', 'technology');
-//}
+// Create the Technology Category Widget
+class milly_technology_cat_widget extends WP_Widget {
+  function milly_technology_cat_widget() {
+    $milly_technology_cat_widget_name = __('Technology Category Widget');
+    $milly_technology_cat_widget_description = __('Displays Technology Category.');
+    $widget_ops = array('classname' => 'milly_technology_cat_widget', 'description' => $milly_technology_widget_cat_description );
+    $this->WP_Widget('milly_technology_cat_widget', $milly_technology_cat_widget_name, $widget_ops);
+  }  
+  
+  function widget($args, $instance) { 
+    extract($args);
+    $widget_title = strip_tags($instance['widget_title']);
+    echo "{$before_widget}{$before_title}$widget_title{$after_title}<div class='technology_cat'>";
+    $args=array(
+	'orderby' => 'name',
+	'order' => 'ASC',
+	'show_count' => 1,
+	'hide_empty' => 1,
+	'title_li' => '',
+	'taxonomy' => 'tech_category'
+    );
+    wp_list_categories($args);
+    echo "</div>";
+    echo $after_widget;
+  }
+  
+  function update($new_instance, $old_instance) {
+    $instance = $old_instance;
+    $instance['widget_title'] = strip_tags($new_instance['widget_title']);
+    return $instance;
+  }
+  
+  function form($instance) {
+    $widget_title = strip_tags($instance['widget_title']);
+    ?><p><label for="<?php echo $this->get_field_id('widget_title'); ?>"><?php _e('Widget Title')?>:<input class="widefat" id="<?php echo $this->get_field_id('widget_title'); ?>" name="<?php echo $this->get_field_name('widget_title'); ?>" type="text" value="<?php echo attribute_escape($widget_title); ?>" /></label></p><?php
+  }
+}
+
+add_action('widgets_init', 'milly_technology_cat_widget_init');
+function milly_technology_cat_widget_init() {
+        register_widget('milly_technology_cat_widget');
+}
+
+// Create the Technology Tag Widget
+class milly_technology_tag_widget extends WP_Widget {
+  function milly_technology_tag_widget() {
+    $milly_technology_tag_widget_name = __('Technology Tag Widget');
+    $milly_technology_tag_widget_description = __('Displays Technology Tags.');
+    $widget_ops = array('classname' => 'milly_technology_tag_widget', 'description' => $milly_technology_widget_tag_description );
+    $this->WP_Widget('milly_technology_tag_widget', $milly_technology_tag_widget_name, $widget_ops);
+  }  
+  
+  function widget($args, $instance) { 
+    extract($args);
+    $widget_title = strip_tags($instance['widget_title']);
+    echo "{$before_widget}{$before_title}$widget_title{$after_title}<div class='technology_tags'>";
+    wp_tag_cloud(array('taxonomy' => 'tech_tag'));
+    echo "</div>";
+    echo $after_widget;
+  }
+  
+  function update($new_instance, $old_instance) {
+    $instance = $old_instance;
+    $instance['widget_title'] = strip_tags($new_instance['widget_title']);
+    return $instance;
+  }
+  
+  function form($instance) {
+    $widget_title = strip_tags($instance['widget_title']);
+    ?><p><label for="<?php echo $this->get_field_id('widget_title'); ?>"><?php _e('Widget Title')?>:<input class="widefat" id="<?php echo $this->get_field_id('widget_title'); ?>" name="<?php echo $this->get_field_name('widget_title'); ?>" type="text" value="<?php echo attribute_escape($widget_title); ?>" /></label></p><?php
+  }
+}
+
+add_action('widgets_init', 'milly_technology_tag_widget_init');
+function milly_technology_tag_widget_init() {
+        register_widget('milly_technology_tag_widget');
+}
 
 // Change all post in category Technology to post type technology
 function technology_post_type_convert() {
