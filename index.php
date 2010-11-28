3ab0747ee1da2b0761cdbcf78bbf5843d99213e2
Matt Rude <m@mattrude.com>
2010-11-27 22:23:27 -0600

    remove set_post_format as it's not in 3.0
    fixes #3

--- index.php
+++ index.php
@@ -20,10 +20,8 @@
 				}
 			} else {
 				if ( in_category( 'gallery' ) ) {
-					set_post_format( $post->ID, 'gallery');
 					include( 'functions/gallery-index.php' );
                                 } elseif ( in_category( 'aside' ) ) {
-					set_post_format( $post->ID, 'aside' );
                                         milly_post_aside();
 				} else {
 				        milly_post_full();
