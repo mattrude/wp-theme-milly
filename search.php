821dde4b8a7806e8162b5ac82ed14a671df17c27
Matt Rude <m@mattrude.com>
2010-11-28 19:09:35 -0600

    bring search function up to ticket #4, see #4

--- search.php
+++ search.php
@@ -7,14 +7,22 @@
 			<h1>Search Results for: <?php the_search_query(); ?></h1>
 		</div>
 		<?php while (have_posts()) : the_post(); ?>
-			<div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+			<div class="post" id="post-<?php the_ID(); ?>">
 				<h1 class="single-title entry-title">
 					<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
 				</h1>
 				<p class="byline">
 					<span class="byline-prep byline-prep-author text">Posted on </span>
 					<span class="published"><?php the_time('F jS, Y') ?></span>
-					<span >filed under <?php the_category(', ') ?> | <?php the_tags('Tags: ', ', ', ''); ?><?php edit_post_link(' | Edit', ''); ?></span>
+					<?php if ( get_post_type() == 'technology' ) {
+					    echo "<span>";
+					    echo get_the_term_list( $post->ID, 'tech_category', ' | Filed Under: ', ', ', '' );
+					    echo get_the_term_list( $post->ID, 'tech_tag', ' | Tagged as: ', ', ', '' );
+					    echo "</span>";
+				        } elseif ( get_post_type() == 'post' ) { ?>
+					    <span > | Filed Under: <?php the_category(', '); the_tags(' | Tagged as: ', ', ', ''); ?></span>
+					<?php } ?>
+					<span ><?php edit_post_link(' | Edit', ''); ?></span>
 				</p>
 			</div><!--close post class and post# id-->
 		<?php endwhile; ?>
