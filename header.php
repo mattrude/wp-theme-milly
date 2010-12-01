48b34c102a74d4db42c68013f8130fef66fa002c
Matt Rude <m@mattrude.com>
2010-12-01 13:27:19 -0600

    Revert "Added <?php body_class(); ?> to each header's <body> tag."
    Wait until I figure out how to handle the CSS for this change.
    
    This reverts commit f35462d521c2ef42f685dd9bdd47dd98cf145bc7.

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
   <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body>
   <?php if (function_exists('google_analytics')) { google_analytics(); } ?>
   <div id="wrapper" >
     <div id="header-title">
