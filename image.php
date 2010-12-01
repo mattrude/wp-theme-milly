22b3143c3923825d1e1bff25753a014812936be1
Matt Rude <m@mattrude.com>
2010-11-30 22:14:44 -0600

    put the tagthis entries in their own <div>.

--- image.php
+++ image.php
@@ -47,10 +47,12 @@
                 </div>
 	</div>
 	<p><a class="wrapper" href="<?php echo get_permalink($post->post_parent); ?>" rev="up post">&larr; <?php printf(__('back to &#8220;%s&#8221;', 'mdr-milly-theme'), get_the_title($post->post_parent)); ?></a></p>
-	<h3 class="comment-title exif-title">Who is this?</h3>	
-	<div id="tagthis"></div>
-	<p><?php echo get_the_term_list( $post->ID, 'people', '<br />Already Tagged: ', ', ', '<br />' ); ?></p>
 </div><!--close entry class-->
+	<div id="community-tags">
+		<h3 class="comment-title exif-title">Who is this?</h3>	
+		<div id="tagthis"></div>
+		<p><?php echo get_the_term_list( $post->ID, 'people', '<br />Already Tagged: ', ', ', '<br />' ); ?></p>
+	</div>
         <?php mdr_exif(); ?>
 	<br />
 </div><!--close post class & post# id-->
