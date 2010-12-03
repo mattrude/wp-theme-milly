6778cfadc8069d87b1db43cd69e288a209286974
Matt Rude <m@mattrude.com>
2010-12-03 01:07:14 -0600

    Put back the notice if no sidebar is set

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,14 @@
-<?php 
-echo "<div id='primary' class='aside'>";
-if ( get_post_type($post->ID) == 'technology' ) {
-         dynamic_sidebar('technology-widget-area');
-} else {
+<div id="primary" class="aside">
+	<?php
  	/* Widgetized sidebar, if you have the plugin installed. */
-	dynamic_sidebar('sidebar-widget-area');
-}
-echo "</div>";
+	if ( is_active_sidebar( 'sidebar-widget-area') ) {
+		dynamic_sidebar( 'sidebar-widget-area');
+	} else { ?>
+		<div id="primary" class="aside">
+		<div class="widget bookmarks widget-bookmarks">
+		Welcome to your new site!<br />
+		To add items to your sidebar, go to the <a href="<?php bloginfo('url'); ?>/wp-admin/widgets.php">widgets page</a> from your admin Dashboard.	
+		</div>
+		</div>
+	<?php } ?>
+</div>
