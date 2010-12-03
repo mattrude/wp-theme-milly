efc916bee1012303675b4acdf84ab33efa9e332c
Matt Rude <m@mattrude.com>
2010-12-03 17:05:03 -0600

    Fixed some problems, See #13

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
+<form method="get" id="searchform" action="<?php echo home_url(); ?>/">
  <div>
   <input type="text" name="s" id="s" onblur="if ( value == '' ) { this.value='Search Pages' }" onfocus="if ( value == 'Search Pages' ) { this.value='' }" tabindex="1" size="25 em" value="Search Pages"/>
   <input type="hidden" id="searchsubmit" value="Search" />
