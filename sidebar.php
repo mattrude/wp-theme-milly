145dc3acb06420b810ebe0f989e44fb3787614b7
Matt Rude <m@mattrude.com>
2010-12-03 17:04:34 -0600

    Fixed some problems, See #13

--- sidebar.php
+++ sidebar.php
@@ -7,7 +7,7 @@
 			<div class="widget bookmarks widget-bookmarks">
 				<p><?php _e('Welcome to your new site!'); ?></p>
                			<p><?php _e('This is your Technology Widget area.  To add items to this sidebar, go to the'); ?>
-				<a href="<?php bloginfo('url'); ?>/wp-admin/widgets.php"><?php _e('widgets page'); ?></a>
+				<a href="<?php echo home_url(); ?>/wp-admin/widgets.php"><?php _e('widgets page'); ?></a>
 				<?php _e("from your admin Dashboard, and add items to the 'Technology Widget Area' on the right hand side."); ?></p>
 			</div> <?php
 		}
@@ -18,7 +18,7 @@
 			<div class="widget bookmarks widget-bookmarks">
 				<p><?php _e('Welcome to your new site!'); ?></p>
 				<p><?php _e('To add items to your sidebar, go to the'); ?>
-				<a href="<?php bloginfo('url'); ?>/wp-admin/widgets.php"><?php _e('widgets page'); ?></a>
+				<a href="<?php echo home_url(); ?>/wp-admin/widgets.php"><?php _e('widgets page'); ?></a>
 				<?php _e("from your admin Dashboard, and add items to the 'Sidebar Widget Area' on the right hand side."); ?></p>	
 			</div> <?php
 		}
