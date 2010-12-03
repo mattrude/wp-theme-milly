b9d892c4d023d43b2a50acb760fa7dc565384ad3
Matt Rude <m@mattrude.com>
2010-12-03 13:38:01 -0600

    Add Translation strings to 404.php, See #10

--- 404.php
+++ 404.php
@@ -2,8 +2,8 @@
 
 <div id="content">
   <div class="page">
-    <h2 class="center"><center>Oops! - 404 Error</center></h2> 
-    <p class="center"><center>No sense sticking around here for very long. Check out the sidebar where you will be able to search, or checkout my <a href="http://mattrude.com/category/gallery/">Photo Gallery</a> or <a href="http://mattrude.com/category/tech/">Technology</a> section if your into those sorts of things. If you feel you've reached this page as an error on my part, please <a href="http://mattrude.com/contact-me/">contact me</a>.</center></p>
+    <h2 class="center"><center><?php _e('Oops! - 404 Error'); ?></center></h2> 
+    <p class="center"><center><?php _e('No sense sticking around here for very long. Check out the sidebar where you will be able to search, or checkout my'); ?> <a href="<?php bloginfo('url'); ?>/category/gallery/"><?php _e('Photo Gallery'); ?></a> <?php _e('or'); ?> <a href="<?php bloginfo('url'); ?>/technology/"><?php _e('Technology'); ?></a> <?php _e('section if your into those sorts of things. If you feel you\'ve reached this page as an error on my part, please'); ?> <a href="<?php bloginfo('url'); ?>/contact-me/"><?php _e('contact me'); ?></a>.</center></p>
     <center><img src="<?php bloginfo('template_url') ?>/images/404.jpg"></center>
 
   </div>
