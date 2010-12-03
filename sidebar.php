4d9a2fd295163147c0df6e98120cfa7617e72baa
Matt Rude <m@mattrude.com>
2010-12-03 01:34:22 -0600

    sidebar.php code cleanup.

--- sidebar.php
+++ sidebar.php
@@ -1,14 +1,26 @@
 <div id="primary" class="aside">
 	<?php
- 	/* Widgetized sidebar, if you have the plugin installed. */
-	if ( is_active_sidebar( 'sidebar-widget-area') ) {
-		dynamic_sidebar( 'sidebar-widget-area');
-	} else { ?>
-		<div id="primary" class="aside">
-		<div class="widget bookmarks widget-bookmarks">
-		Welcome to your new site!<br />
-		To add items to your sidebar, go to the <a href="<?php bloginfo('url'); ?>/wp-admin/widgets.php">widgets page</a> from your admin Dashboard.	
-		</div>
-		</div>
-	<?php } ?>
+	if ( get_post_type($post->ID) == 'technology' ) {
+		if ( is_active_sidebar( 'technology-widget-area' ) ) {
+			dynamic_sidebar( 'technology-widget-area' );
+        	} else { ?>
+			<div class="widget bookmarks widget-bookmarks">
+				<p>Welcome to your new site!</p>
+               			<p>This is your Technology Widget area.  To add items to this sidebar, go to the
+				<a href="<?php bloginfo('url'); ?>/wp-admin/widgets.php">widgets page</a>
+				from your admin Dashboard, and add items to the 'Technology Widget Area' on the right hand side.</p>
+			</div> <?php
+		}
+	} else {
+		if ( is_active_sidebar( 'sidebar-widget-area' ) ) {
+	    		dynamic_sidebar( 'sidebar-widget-area' );
+		} else { ?>
+			<div class="widget bookmarks widget-bookmarks">
+				<p>Welcome to your new site!</p>
+				<p>To add items to your sidebar, go to the
+				<a href="<?php bloginfo('url'); ?>/wp-admin/widgets.php">widgets page</a>
+				from your admin Dashboard, and add items to the 'Sidebar Widget Area' on the right hand side.</p>	
+			</div> <?php
+		}
+	} ?>
 </div>
