a27a357e88f8d1285bc6f76a188ed183d1c47277
Matt Rude <matt@mattrude.com>
2011-10-06 23:09:00 +0000

    Add google Voice Search, fixes #17

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form method="get" id="searchform" action="<?php echo home_url(); ?>/">
  <div>
-  <input type="text" name="s" id="s" onblur="if ( value == '' ) { this.value='Search Pages' }" onfocus="if ( value == 'Search Pages' ) { this.value='' }" tabindex="1" size="25 em" value="Search Pages"/>
+  <input type="text" name="s" id="s" onblur="if ( value == '' ) { this.value='Search Pages' }" onfocus="if ( value == 'Search Pages' ) { this.value='' }" tabindex="1" size="25 em" value="Search Pages" x-webkit-speech speech onwebkitspeechchange="this.form.submit();" />
   <input type="hidden" id="searchsubmit" value="Search" />
  </div>
 </form>
