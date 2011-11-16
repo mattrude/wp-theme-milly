c43a0b151ed60aed794236db210c4c889fb4450f
Matt Rude <matt@mattrude.com>
2011-11-16 16:15:30 -0600

    updating the twitter image functions.

--- archive-twitter.php
+++ archive-twitter.php
@@ -5,7 +5,14 @@ $twitterid = $Panel->Settings('TwitterID');
 $twittername = $Panel->Settings('TwitterName');
 $twittercount = $Panel->Settings('TwitterCount');
 $twitterimgenabled = $Panel->Settings('TwitterImgEnabled');
-$t=new twitterImage($twitterid);
+
+$twitter_image_url = wp_cache_get( 'twitter_image_url' );
+if ( false == $twitter_image_url ) {
+  $twitterid = $ozh_ta['screen_name'];
+  $xml = simplexml_load_file("http://twitter.com/users/".$twitterid.".xml");
+  $twitter_image_url = (string)$xml->profile_image_url;
+  wp_cache_set( 'twitter_image_url', $twitter_image_url );
+}
 
 ?>
 <div id="content">
@@ -14,12 +21,7 @@ $t=new twitterImage($twitterid);
 		<?php while (have_posts()) : the_post(); ?>
   <div class="post" id="tweet_template-<?php echo $post->ID; ?>">
     <div id='tweet-<?php echo $post->ID; ?>' class='tweet_post' >
-    <?php
-        if ( $twitterimgenabled == 'true' ) {
-          $t->profile_image(true,true);
-        } else {
-           ?><div class="twitter-avatar"><?php echo get_avatar( get_the_author_meta( 'ID' ), apply_filters( 'twentyeleven_status_avatar', '65' ) ); ?></div><?php
-        }
+    <?php echo "<div class='twitter-avatar'><img src=$twitter_image_url></div>";
       the_content(); ?>
     </div>
       <?php milly_twitter_byline(); ?>
