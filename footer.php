48a2e3a7778d43a3a04a86f7d15dc221ca74a340
Matt Rude <matt@mattrude.com>
2011-11-23 12:55:28 -0600

    remove unused settings in footer.php

--- footer.php
+++ footer.php
@@ -7,13 +7,6 @@
 		<div id="footer-logo">
 			<a href="http://wordpress.org/" rel="nofollow"><img src="<?php echo get_template_directory_uri(); ?>/images/wordpress-logo.png" height='100' width='100' alt="WordPress Logo" /></a>
 		</div>
-		<?php global $Panel;
-		$copyright = $Panel->Settings('copyenable');
-		if ($copyright == 'true') { ?>
-			<div id="footer-copyright">
-				<a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/" rel="nofollow"><img alt="Creative Commons Attribution-Share Alike 3.0 Unported License" style="border-width:0" src="<?php echo get_template_directory_uri(); ?>/images/cc-by-sa.png" /></a>
-			</div>
-		<?php } ?>
 		<div id="footer-text">
 			<p>
 			Copyright &copy; 1980 – <?php echo date("Y") ?> by <a href="http://mattrude.com/">Matt Rude</a><br />
@@ -21,7 +14,6 @@
 			Proudly powered by <a href="http://wordpress.org/" rel="nofollow">WordPress</a>.<br />
 			This page took <?php timer_stop(1); ?> seconds of computer labor,<br/>
 			and required <?php echo get_num_queries(); ?> questions to produce.<br />
-			<?php global $Panel; echo $Panel->Settings('FooterText'); ?><br />
 			</p>
 		</div>
 	</div>
