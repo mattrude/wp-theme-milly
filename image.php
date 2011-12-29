2902b5c0045a99c32a556b566c0bb80291fe6a24
mattrude <m@mattrude.com>
2011-12-28 20:31:04 -0600

    whitespace cleanup

--- image.php
+++ image.php
@@ -4,69 +4,62 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<?php if ( ! get_post_meta($post->ID, 'sharing_disabled') ) { add_post_meta($post->ID, 'sharing_disabled', '1'); }; ?>
-	<div class="single">
-		<?php if ( !empty($post->post_excerpt) ) {
+	<?php if ( !empty($post->post_excerpt) ) {
 		  echo "<div class='post'><div class='comment-title'><h2>";
 		  the_excerpt();
 		  echo "</h2></div>";
 		  the_content();
 		  echo "</div>" ;// this is the "caption"
-		} ?>
-		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<small class="attr"><?php the_time('F jS, Y') ?></small>
-			<!--<h1><?php the_title(); ?></h1>-->
-			<div class="image_entry">
-				<?php $image_full_url = wp_get_attachment_image_src( $post->ID, "full" ); ?>
-				<p class="attachment"><a href="<?php echo $image_full_url[0]; ?>"><?php echo wp_get_attachment_image( $post->ID, array(894,894) ); ?></a></p>
-				<div class="caption"><?php if ( !empty($post->post_excerpt) ) the_excerpt(); // this is the "caption" ?></div>
-	<div class="image-navigation">
-		<div class="floatright">
-		<?php $attachments = array_values(get_children( array('post_parent' => $post->post_parent, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID') ));
-			foreach ( $attachments as $k => $attachment )
-			  if ( $attachment->ID == $post->ID )
-			    break;
-		$attachments = array_values(get_children( array('post_parent' => $post->post_parent, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID') ));
+	} ?>
+	<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<small class="attr"><?php the_time('F jS, Y') ?></small>
+		<?php $image_full_url = wp_get_attachment_image_src( $post->ID, "full" ); ?>
+		<p class="attachment"><a href="<?php echo $image_full_url[0]; ?>"><?php echo wp_get_attachment_image( $post->ID, array(894,894) ); ?></a></p>
+		<div class="image-meta">
+			<div class="caption"><?php if ( !empty($post->post_excerpt) ) the_excerpt(); // this is the "caption" ?></div>
+				<div class="floatright">
+					<?php $attachments = array_values(get_children( array('post_parent' => $post->post_parent, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID') ));
+					foreach ( $attachments as $k => $attachment )
+					  if ( $attachment->ID == $post->ID )
+					    break;
 
-		$next_url =  isset($attachments[$k+1]) ? get_permalink($attachments[$k+1]->ID) : get_permalink($attachments[0]->ID);
-		$previous_url =  isset($attachments[$k-1]) ? get_permalink($attachments[$k-1]->ID) : get_permalink($attachments[0]->ID);
-		if ( wp_get_attachment_image( $post->ID+1 ) != null ) { ?>
-		<p class="attachment">
-			<?php _e('Next Image', 'milly') ?><br />
-			<a href="<?php echo $next_url; ?>"><?php echo wp_get_attachment_image( $post->ID+1, 'thumbnail' ); ?></a>
-		</p>
-		<?php }
+					$attachments = array_values(get_children( array('post_parent' => $post->post_parent, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID') ));
+					$next_url =  isset($attachments[$k+1]) ? get_permalink($attachments[$k+1]->ID) : get_permalink($attachments[0]->ID);
+					$previous_url =  isset($attachments[$k-1]) ? get_permalink($attachments[$k-1]->ID) : get_permalink($attachments[0]->ID);
+					if ( wp_get_attachment_image( $post->ID+1 ) != null ) { ?>
+						<p class="attachment">
+							<?php _e('Next Image', 'milly') ?><br />
+							<a href="<?php echo $next_url; ?>"><?php echo wp_get_attachment_image( $post->ID+1, 'thumbnail' ); ?></a>
+						</p>
+					<?php }
 
-		if ( wp_get_attachment_image( $post->ID-1 ) != null ) { ?>
-		<p class="attachment">
-			<?php _e('Previous Image', 'milly') ?><br />
-			<a href="<?php echo $previous_url; ?>"><?php echo wp_get_attachment_image( $post->ID-1, 'thumbnail' ); ?></a>
-		</p>
-		<?php } ?>
-		<div class="floatright">
-			<?php edit_post_link('Edit Image'); ?>
-                </div>
+					if ( wp_get_attachment_image( $post->ID-1 ) != null ) { ?>
+						<p class="attachment">
+							<?php _e('Previous Image', 'milly') ?><br />
+							<a href="<?php echo $previous_url; ?>"><?php echo wp_get_attachment_image( $post->ID-1, 'thumbnail' ); ?></a>
+						</p>
+					<?php } ?>
+					<?php edit_post_link('Edit Image'); ?>
+                		</div>
+				<p><a class="wrapper" href="<?php echo get_permalink($post->post_parent); ?>" rev="up post">&larr; <?php printf(__('back to &#8220;%s&#8221;', 'mdr-milly-theme'), get_the_title($post->post_parent)); ?></a></p>
+				<div id="community-tags">
+				<h3 class="comment-title exif-title">Who is this?</h3>	
+				<div id="tagthis"></div>
+				<p><?php echo get_the_term_list( $post->ID, 'people', '<br />' .__('Already Tagged', 'milly'). ': ', ', ', '' ); ?></p>
+			</div><!--close caption id-->
+		        <?php mdr_exif(); ?>
+			<br />
+		</div><!--close post class & post# id-->
+		<div id="image-comments" >
+		<?php comments_template(); ?>
 	</div>
-	<p><a class="wrapper" href="<?php echo get_permalink($post->post_parent); ?>" rev="up post">&larr; <?php printf(__('back to &#8220;%s&#8221;', 'mdr-milly-theme'), get_the_title($post->post_parent)); ?></a></p>
-</div><!--close entry class-->
-	<div id="community-tags">
-		<h3 class="comment-title exif-title">Who is this?</h3>	
-		<div id="tagthis"></div>
-		<p><?php echo get_the_term_list( $post->ID, 'people', '<br />' .__('Already Tagged', 'milly'). ': ', ', ', '' ); ?></p>
-	</div>
-        <?php mdr_exif(); ?>
 	<br />
-</div><!--close post class & post# id-->
-<div id="image-comments" >
-  <?php comments_template(); ?>
-</div>
-<br />
-<div id='random-images'>
-  <?php // random_images() . random_images() . random_images() . random_images(); ?>
-</div>
-</div>
-</div><!--close single class-->
+</div><!--close image_content id-->
+
 <?php endwhile; else: ?>
 	<p><?php _e('Sorry, no posts matched your criteria.', 'milly') ?></p>
 <?php endif; ?>
+
 </div><!--close content id-->
+
 <?php get_footer(); ?>
